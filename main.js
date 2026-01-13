86c5a48b4a62bd909896df3e327cb79297dc1f70
Saulo Faria <saulocmf@gmail.com>
2026-01-13 15:01:38 +0100

    premierVersion

---
+++ main.js
@@ -0,0 +1,106 @@
+import './style.css';
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import { RegularShape, Fill, Stroke, Style } from 'ol/style';
+
+// Creat a marker style
+const starStyle = new Style({
+  image: new RegularShape({
+    fill: new Fill({ color: 'gold' }),
+    stroke: new Stroke({ color: 'black', width: 2 }),
+    points: 5, 
+    radius: 9,     
+    radius2: 5,     
+  })
+});
+
+// Create a list of points
+const listeForros = [
+  {
+    id: 1,
+    name: "Baragones",
+    geometry: new Point([537424.7867,5742889.4701]), // Coords en EPSG: 3857
+    time: "20h",
+    frequence: "1 fois par semaine",
+    saison: "toute l'année",
+  },{
+    id: 2,
+    name: "Place de Jacobins",
+    geometry: new Point(fromLonLat([4.8327698,45.7605099])), // Coords en EPSG: 4326
+    time: "19h",
+    frequence: "1 fois par semaine",
+    saison: "été"
+  },{
+    id: 3,
+    name: "Toi Toi le zinc",
+    geometry: new Point(fromLonLat([4.8769956,45.7798382])), // Coords en EPSG: 4326
+    time: "21h",
+    frequence:"1 fois par mois",
+    saison: "hiver/printemps"
+  }
+]
+
+// map(loop) through the list to create a feature
+const forroPtFeatures = listeForros.map(point => {
+  return new Feature ({
+    geometry: point.geometry,
+    name: point.name,
+    time: point.time,
+    frequence: point.frequence,
+    saison: point.saison
+  })
+})
+
+
+// COnstruit un layer a partir des points 
+const vectorSource = new VectorSource({
+  features: forroPtFeatures,
+});
+
+// Construit la couche vectoriel pour la carte a partir du vector source et l'habille avec un style
+const vectorLayer = new VectorLayer({
+  source: vectorSource,
+  style:starStyle
+});
+
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    }),
+    vectorLayer
+  ],
+  view: new View({
+    center: fromLonLat([4.85, 45.77]),
+    zoom: 13,
+  })
+});
+
+// Returns event information when click on point
+map.on('click',function (event) {
+  const feature = map.forEachFeatureAtPixel(event.pixel, function(feat) {
+    return feat;
+  });
+  // const feature = map.forEachFeatureAtPixel(event.pixel, feat => feat);
+  if (feature) {
+      console.log("Point clicked!!");
+      const name = feature.get('name');
+      const time = feature.get('time');
+
+    alert(`Location: ${name}\nTime: ${time}`);
+  }
+  });
+
+// change mouse cursor when over marker
+map.on('pointermove', function (event) {
+  const hit = map.hasFeatureAtPixel(event.pixel);
+  map.getTargetElement().style.cursor = hit ? 'pointer' : '';
+});
